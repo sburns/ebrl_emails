a16583f85efe986bd0598e1e6903c203b3a34de1
Scott Burns <scott.s.burns@gmail.com>
2011-11-11 13:15:44 -0600

    Little touches to RCV email

--- bin/rcv.py
+++ bin/rcv.py
@@ -3,6 +3,8 @@
 __author__ = 'Scott Burns <scott.s.burns@gmail.com>'
 __license__ = 'BSD 3-Clause'
 
+
+import re
 import time
 
 from redcap import Project
@@ -10,7 +12,8 @@ from ebrl.mail import mail
 from ebrl.config import rc as redcap_keys
 
 URL = 'https://redcap.vanderbilt.edu/api/'
-TO = ('scott.s.burns@vanderbilt.edu', 'nikki.davis@vanderbilt.edu')
+TO = ('scott.s.burns@vanderbilt.edu','heather.c.harris@vanderbilt.edu', 'nikki.davis@vanderbilt.edu')
+#TO = ('scott.s.burns@vanderbilt.edu',)
 
 if __name__ == '__main__':
 
@@ -26,18 +29,22 @@ if __name__ == '__main__':
 
     body = """<html><body><h2>RCV Redcap database as of {time}</h2>
     
-<table>
-<tr>
-<td>{0:15s}</td><td>{1:15s}</td><td>{2:15s}</td>
+<table border="1">
+<tr><th>{0}</th><th>{1}</th><th>{2}</th></tr>
 {d}
 </table>
+<p> Total scans: {tot_scans} </p>
 </body></html>
     """
     
-    d = '\n'.join(["<tr><td>{0:15s}</td><td>{1:15s}</td><td>{2:15s}</td></tr>".format(x['participant_id'], 
+    d = '\n'.join(["<tr><td>{0}</td><td>{1}</td><td>{2}</td></tr>".format(x['participant_id'], 
         x['scan_num'], x['im_date']) for x in data])
     
-    body_to_send = body.format('ID', 'Scan Num', 'Scan Date', d=d, time=time.strftime('%A, %d %b %Y'))
+    tot_subs = len(d)
+    tot_scans = len([x for x in data if re.match('(\d){4,6}', x['scan_num'])])
+    
+    body_to_send = body.format('ID', 'Scan Num', 'Scan Date', d=d,
+        time=time.strftime('%A, %d %b %Y'), tot_scans=tot_scans)
     subject = "[EBRL Automated Email] Reading Comprehension Redcap (Imaging)"
     #  print body_to_send
-    mail(TO, subject, body_to_send)
+    mail(TO, subject, body_to_send, body_type='html')
