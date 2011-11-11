1626eeceec5ed4e39e620c65f4d29da6c52db44a
Scott Burns <scott.s.burns@gmail.com>
2011-11-11 12:39:37 -0600

    Fix cfg file bug

--- ebrl/config.py
+++ ebrl/config.py
@@ -3,10 +3,17 @@
 __license__ = 'BSD 3-Clause'
 
 import os
+import sys
 from ConfigParser import ConfigParser
 
+platform = os.uname()[0]
+if platform == 'Linux':
+    cfg_file = '/fs0/.ebrl.cfg'
+else:
+    cfg_file = os.path.join(os.path.expanduser('~'), '.ebrl.cfg')
+
 cfg = ConfigParser()
-cfg.read(os.path.join(os.path.expanduser('~'), '.ebrl.cfg'))
+cfg.read(cfg_File)
 
 def tl2dict(tl):
     """ Takes a list of 2-tuples and returns a dict """
